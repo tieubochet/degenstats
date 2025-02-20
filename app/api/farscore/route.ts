d2e0576f3bc98ef11f8d43f42883b0d8904d409f
tieubochet <tao.nguyen712@gmail.com>
2025-02-20 11:20:38 +0700

    remove some warning

--- app/api/farscore/route.ts
+++ app/api/farscore/route.ts
@@ -7,7 +7,7 @@ if (!apiKey) {
 }
 init(apiKey);
 
-console.log("Airstack API initialized");
+//console.log("Airstack API initialized");
 
 const userQuery = `
 query GetUserSocialCapital($userId: String!) {
@@ -35,8 +35,8 @@ query GetUserSocialCapital($userId: String!) {
 `;
 
 export async function GET(req: NextRequest) {
-  console.log(`API route called at ${new Date().toISOString()}`);
-  console.log(`Full URL: ${req.url}`);
+  //console.log(`API route called at ${new Date().toISOString()}`);
+  //console.log(`Full URL: ${req.url}`);
 
   const userId = req.nextUrl.searchParams.get("userId");
   console.log(`Requested userId: ${userId}`);
